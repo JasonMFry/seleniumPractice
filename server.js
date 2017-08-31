55b26ac92d20d7a54b72ef500d17769b89325335
Jason Fry <jason@feathr.co>
2017-08-31 11:56:02 -0400

    Take a screenshot and save it to current folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const webdriver = require('selenium-webdriver');
 const By = webdriver.By;
 const until = webdriver.until;
+const fs = require('fs');
 
 // open Firefox
 const driver = new webdriver.Builder().forBrowser('firefox').build();
@@ -8,39 +9,9 @@ const driver = new webdriver.Builder().forBrowser('firefox').build();
 driver.manage().window().maximize();
 // delete all cookies
 driver.manage().deleteAllCookies();
-// helper functions
-function findLink(link) {
-    return driver.findElements(webdriver.By.css(`[href="${link}"]`))
-        .then((results) => {
-            return results[0];
-        });
-}
 
-function clickLink(link) {
-    link.click();
-}
-
-function logTitle() {
-    driver.sleep(1000);
-    driver.getTitle().then((title) => {
-        console.log(`Current page title is ${title}`)
-    });
-}
-
-function closeBrowser() {
-    driver.quit();
-}
-
-function handleFailure(err) {
-    console.log(`Something went wrong: ${err.stack}`);
-    closeBrowser();
-}
-
-// navigate
-driver.get('https://www.google.com/');
-driver.findElement(webdriver.By.name('q')).sendKeys('iolearn Video Tutorials');
-driver.findElement(webdriver.By.name('btnK')).click();
-driver.wait(findLink("https://iolearn.com/"), 2000)
-    .then(clickLink)
-    .then(logTitle)
-    .then(closeBrowser, handleFailure);
+driver.get('https://iolearn.com');
+driver.takeScreenshot().then((data) => {
+    fs.writeFileSync('img.png', data, 'base64');
+});
+driver.quit();
